a456998508f55d0583bc50bc1c46a5861b1ea9f5
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 18:22:02 +0100

    ex02

---
+++ Repo04/ex02/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    var myRes = 75;
+    // only change code below this line 
+    myRes++;
+    // only change code above this line
+    return myRes;
+}
+
+console.log(main());
+module.exports = main;
