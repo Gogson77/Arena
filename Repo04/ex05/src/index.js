bfacc8e196776b2c2d4e86e8778b2d298a9298ee
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 18:31:19 +0100

    ex05

---
+++ Repo04/ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // only change code below this line 
+    product = numOne * numTwo;
+    // only change code above this line
+    return product;
+}
+
+console.log(main(2.5, 2.0));
+console.log(main(5.5, 5.5));
+console.log(main(100.25, 43.20));
+module.exports = main;
