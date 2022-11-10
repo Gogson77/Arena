810400f98211e7c73a808f502340e8087441c1c5
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:25:33 +0100

    ex05

---
+++ Repo03/ex05/src/index.js
@@ -0,0 +1,9 @@
+function main(){
+    // Only change the code below this line
+    var x = 8;
+    // Only change the code below this line
+    return x;
+}
+
+console.log(main());
+module.exports = main;
