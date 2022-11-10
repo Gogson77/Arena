0f7806ea80dd95bf4724a3f3a2987f366c30d5b5
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 10:57:00 +0100

    single and multi line comment

---
+++ Repo03/ex02/src/index.js
@@ -0,0 +1,20 @@
+function main() {
+    // Only change code below this line
+
+    // this single line comment has at least five letters - a single line Javascript comment
+
+    /* this multi line comment has at least five letters and five rows 
+    document.write("This is a multi-line comment!")
+    document.write("This is a multi-line comment!")
+    document.write("This is a multi-line comment!")
+    document.write("This is a multi-line comment!")
+    document.write("This is a multi-line comment!")
+    */
+
+    // Only change code above this line
+
+}
+
+console.log(main());
+
+module.export = main;
