353327932455278b9ee6b4601e133a39a795fecd
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:49:10 +0100

    ex08

---
+++ Repo03/ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    // Only change the code below this line
+    numOne;
+    numTwo;
+    sum = numOne + numTwo;
+    // Only change the code above this line
+    return sum;
+}
+console.log(main(15,8));
+console.log(main(5,28));
+console.log(main(150,425));
+module.exports = main;
