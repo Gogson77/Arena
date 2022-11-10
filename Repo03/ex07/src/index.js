46ed9e4b94966ed1b903ed3bfea85606d79181b2
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:39:21 +0100

    ex07

---
+++ Repo03/ex07/src/index.js
@@ -0,0 +1,20 @@
+function main(){
+    // Only change the code below this line
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+    // Variable assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+    // Only change the code above this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    }
+}
+
+console.log(main());
+module.exports = main;
