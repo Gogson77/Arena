f211bb8ed4a6040809414456f9197c497dff7507
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:59:47 +0100

    ex09

---
+++ Repo03/ex09/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo){
+    // Only change the code below this line
+    var numOne;
+    var numTwo;
+    var difference = numOne - numTwo;
+    // Only change the code above this line
+    return difference;
+}
+console.log(main(15,8));
+console.log(main(25,8));
+console.log(main(425,120));
+module.exports = main;
