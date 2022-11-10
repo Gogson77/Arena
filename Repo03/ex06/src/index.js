90803666a13320144c1fbb3bcaefca93cdd48718
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:31:25 +0100

    ex06

---
+++ Repo03/ex06/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // Only change the code below this line
+    var x = 7;
+    var y = 12;
+    var z = "My name is";
+    // Only change the code below this line
+    x = x + 2;
+    y = y + 4;
+    z = z + " John!";
+    return[x, y, z];
+}
+
+console.log(main());
+module.exports = main;
