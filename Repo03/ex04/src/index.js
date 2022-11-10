e3179b7cfcba955651139372c522f6b260ac138a
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 11:17:00 +0100

    ex04

---
+++ Repo03/ex04/src/index.js
@@ -0,0 +1,24 @@
+function main(){
+    // Only change the code below this line
+    
+    var drawCats;
+    drawCats = 5;
+    
+    var myCat;
+    myCat = 5;
+
+    var ourCats;
+    ourCats = myCat;
+    
+
+    var x;
+    x = 10;
+    var y;
+    y = x;
+    // Only change the code above this line
+    return [x, y];
+}
+
+console.log(main());
+
+module.exports = main;
