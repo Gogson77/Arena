2ae71d7974e179a1e5a1e1037a2710ce43da762d
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 18:28:04 +0100

    ex04

---
+++ Repo04/ex04/src/index.js
@@ -0,0 +1,13 @@
+function main(){
+    // only change code below this line 
+    var myDecimal = 2.54;
+    var myDecimalE = myDecimal * 10e-4;  
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+
+console.log(main());
+module.exports = main;
