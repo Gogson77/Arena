fca7899748d08c40a3c051671f2531c81cbfd448
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 18:35:36 +0100

    ex06

---
+++ Repo04/ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(numOne, numTwo){
+    // only change code below this line 
+    myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0));
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));
+module.exports = main;
