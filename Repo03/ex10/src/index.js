34647ddc66852f3cddd6f9fb2b8de0e22142e2ab
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-10 13:46:37 +0100

    ex10

---
+++ Repo03/ex10/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numTwo){
+    // Only change the code below this line
+    var numOne;
+    var numTwo;
+    product = numOne * numTwo;
+    var product;
+    // Only change the code above this line
+    return product;
+}
+console.log(main(15,3));
+console.log(main(25,8));
+console.log(main(425,10));
+module.export = main;
