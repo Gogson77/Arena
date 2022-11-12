749e15050870680ef739cf64a4ec8bff880c1c79
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 11:44:31 +0100

    ex05

---
+++ Repo05/ex05/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    // only change code below this line 
+    var myString = "This is the first string. " + "This is the second string.";
+    // only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
