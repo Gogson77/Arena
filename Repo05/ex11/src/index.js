47c6f3a9514b158545705cbefc96db2dc17c4e79
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 15:37:46 +0100

    ex11

---
+++ Repo05/ex11/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var myString = "Jello World";
+    myString = "Hello World";
+    // only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
