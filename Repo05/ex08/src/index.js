d9fff323a1684f98b66b74bb33fd766f98218739
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 15:28:20 +0100

    ex08

---
+++ Repo05/ex08/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // only change code below this line 
+    var someAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString += someAdjective;
+    // only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
