639b11cc406c7b2955c80338260a5f26115aa617
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 11:25:48 +0100

    ex03

---
+++ Repo05/ex03/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // only change code below this line 
+    var myLink;
+    myLink = '<a href="http://www.example.com" target="_blank" >Link</a>';
+    // only change code above this line
+    return {
+        myLink
+    };
+}
+console.log(main());
+module.exports = main;
