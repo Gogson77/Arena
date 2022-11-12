1c28a936059302fa988c3e601a9521c4d5ccb37a
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 10:58:07 +0100

    ex02

---
+++ Repo05/ex02/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // only change code below this line 
+    var myFirstString;
+    var mySecondString;
+
+    myFirstString = "I think therefore I \"am\" -- sentient.";
+    mySecondString = "No, you only \"think\", so you aren\'t";
+    // only change code above this line
+    return {
+        myFirstString,
+        mySecondString
+    };
+}
+console.log(main());
+module.exports = main;
