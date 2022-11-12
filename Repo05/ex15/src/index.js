050efb18ad712e5d6f24107f9aee6e7654079a66
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 21:21:14 +0100

    ex15

---
+++ Repo05/ex15/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // only change code below this line 
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = 
+    "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " to chase the cat with no success.";
+    // only change code above this line
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
