1deac9fa2bec0fb9f43b77b48829070ff2d2ed17
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 11:35:27 +0100

    ex04

---
+++ Repo05/ex04/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var myString;
+    myString = 'FirstLine\r\n\tSecondLine\r\nThirdLine';
+    // only change code above this line
+    return myString
+}
+console.log(main());
+module.exports = main;
