6d708a122d5ee1f055673b98fa6938295133ffac
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 15:22:07 +0100

    ex06

---
+++ Repo05/ex06/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var myString = "Concatenation is the process of appending ";
+    myString += "one string to the end of another string.";
+    // only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
