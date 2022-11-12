15059e1e67968d18f1d6f6d94806a70b76ee728a
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 15:32:15 +0100

    ex09

---
+++ Repo05/ex09/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var lastName = "Doe";
+    var lastNameLength = lastName.length;
+    // only change code above this line
+    return lastNameLength;
+}
+console.log(main());
+module.exports = main;
