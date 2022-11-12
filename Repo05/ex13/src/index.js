abad1fa01f831f26fa4f5fc4b20a912e9ec6ca88
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 20:53:23 +0100

    ex13

---
+++ Repo05/ex13/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length - 1];
+    // only change code above this line
+    return lastLetterOfLastName;
+}
+console.log(main());
+module.exports = main;
