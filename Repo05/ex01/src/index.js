bdd24aac3c545d48c412753c42da68f94a29756e
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 10:45:19 +0100

    ex01

---
+++ Repo05/ex01/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    // only change code below this line 
+    var myFirstName;
+    var myLastName;
+    var myHomeTown;
+
+    myFirstName = "Gordan";
+    myLastName = "Kranjcic";
+    myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
