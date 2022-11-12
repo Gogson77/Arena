b2109681992de31ac1bc5f4dc48a880ac825a366
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-12 15:25:23 +0100

    ex07

---
+++ Repo05/ex07/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // only change code below this line 
+    var myName = "Gordan";
+    var myString = "My name is " + myName + " and I am well!";
+    // only change code above this line
+    return myString;
+}
+console.log(main());
+module.exports = main;
