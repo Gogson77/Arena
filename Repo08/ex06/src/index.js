7dc432c4ce1b834b58c423a764dc00169d4a4a93
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 14:19:53 +0100

    ex06

---
+++ Repo08/ex06/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+function xSix(num){
+    return num * 6;
+}
+console.log(xSix(5));
+console.log(xSix(2));
+console.log(xSix(0));
+// Only change code above this line
+module.exports = xSix;
