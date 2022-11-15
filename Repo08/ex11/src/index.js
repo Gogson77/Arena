82b0492fce760ecd165d6af7025199a96206e153
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 15:06:41 +0100

    ex11 base

--- Repo08/ex11/src/index.js
+++ Repo08/ex11/src/index.js
@@ -5,6 +5,6 @@ function convertToInt(str, base){
 console.log(convertToInt("BA", 16));
 console.log(convertToInt("BA", 16));
 console.log(convertToInt("F1", 16));
-console.log(convertToInt("JeffBezos", 2));
+console.log(convertToInt("JeffBezos", 16));
 // Only change code above this line
 module.exports = convertToInt;
