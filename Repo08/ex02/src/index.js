e72e36538fa38b01d936547403010e48d3e2438b
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 10:14:34 +0100

    ex02

---
+++ Repo08/ex02/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+function functionPrintInfo(name, age) {
+    console.log(name, age);
+}
+// Write code above this line
+
+functionPrintInfo("John","23");
+functionPrintInfo("Michael","43");
+
+module.exports = functionPrintInfo;
