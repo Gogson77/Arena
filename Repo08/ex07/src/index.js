3ffa5d2b3d7439e49482be2b29ace8f6dadfaded
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 14:25:54 +0100

    ex07

---
+++ Repo08/ex07/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+var dress = "little dress";
+function myDress(){
+    var dress = "ball gown";
+    return dress;
+}
+myDress();
+//console.log(myDress()); it is not specified to print returned value of local variable dressto console 
+// Only change code above this line
+module.exports = myDress;
