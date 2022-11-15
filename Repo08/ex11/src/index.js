25a9300a86b9df82c2f76c4044fb12ae7970ae4a
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 15:05:36 +0100

    ex11 base 16

--- Repo08/ex11/src/index.js
+++ Repo08/ex11/src/index.js
@@ -2,9 +2,9 @@
 function convertToInt(str, base){
     return parseInt(str, base);
 }
-console.log(convertToInt("BA", 6));
-console.log(convertToInt("BA", 6));
-console.log(convertToInt("F1", 6));
+console.log(convertToInt("BA", 16));
+console.log(convertToInt("BA", 16));
+console.log(convertToInt("F1", 16));
 console.log(convertToInt("JeffBezos", 2));
 // Only change code above this line
 module.exports = convertToInt;
