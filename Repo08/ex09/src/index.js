1c184f5b7313dcad88c4f39eb3625985d6b69585
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 14:34:44 +0100

    ex09

---
+++ Repo08/ex09/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function randomInteger(){
+    return Math.floor(Math.random() * 30);
+}
+console.log(randomInteger());
+// Only change code above this line
+module.exports = randomInteger;
