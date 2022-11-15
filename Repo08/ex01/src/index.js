ac38464ae0b6042464b58d358fa1c70ad88d55e7
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 09:56:42 +0100

    ex01

---
+++ Repo08/ex01/src/index.js
@@ -0,0 +1,9 @@
+// Write code below this line
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+// Write code above this line
+
+reusableFunction();
+
+module.exports = reusableFunction;
