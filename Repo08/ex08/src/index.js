8520d85732860d205614373f7113dc836259d28e
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 14:30:29 +0100

    ex08

---
+++ Repo08/ex08/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+var total = 0;
+function funcWithArg(digit){
+    return (digit + 9) / 5;
+}
+total = funcWithArg(6);
+// Only change code above this line
+console.log(total);
+module.exports = funcWithArg;
