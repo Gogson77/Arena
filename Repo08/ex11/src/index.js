5ffd48b47256c038a9c044fe88cba6d78aa19cf9
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-15 15:00:56 +0100

    ex11

---
+++ Repo08/ex11/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+function convertToInt(str, base){
+    return parseInt(str, base);
+}
+console.log(convertToInt("BA", 6));
+console.log(convertToInt("BA", 6));
+console.log(convertToInt("F1", 6));
+console.log(convertToInt("JeffBezos", 2));
+// Only change code above this line
+module.exports = convertToInt;
