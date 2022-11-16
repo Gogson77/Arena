ba092553f68bf37c7766177eab16093fa21d45c7
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-16 10:38:55 +0100

    ex01

---
+++ Repo09/ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function myTrueBoolean(){
+    return true;
+}
+console.log(myTrueBoolean());
+// Only change code above this line
+module.exports = myTrueBoolean;
