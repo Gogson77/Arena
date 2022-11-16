3d8d710070b8baafbc685654babf117d992e24db
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-16 10:32:38 +0100

    ex02

---
+++ Repo09/ex02/src/index.js
@@ -0,0 +1,12 @@
+// Only change the code below this line
+function myFunction(myParameter){
+ if(myParameter){
+    return "The parameter is true!";
+ } 
+ return "The parameter is false!";
+}
+
+console.log(myFunction(true));
+console.log(myFunction(false));
+// Only change the code above this line
+module.exports = myFunction;
