5616f27de48d77adf14c128b16db6decb54f9e2e
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-17 12:38:29 +0100

    ex13

---
+++ Repo09/ex13/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function isLess(a, b){
+    if(a < b){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+// Only change code above this line
+module.exports = isLess;
