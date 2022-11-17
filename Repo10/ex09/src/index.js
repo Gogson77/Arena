92e48f911b6ce610022a02523ff4cbfdd1d0181c
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-17 19:57:09 +0100

    ex09

--- Repo10/ex09/src/index.js
+++ Repo10/ex09/src/index.js
@@ -3,8 +3,8 @@ function myFunction(myObj, checkProp){
         if(myObj.hasOwnProperty(checkProp)){
             return myObj[checkProp];
         } else { return "Not Found"}
-    }
     // Only change code above this line
+}
 console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
 console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "song"));
 console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre"));
