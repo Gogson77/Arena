3b70f7950e3609fca7491743840c30443b8365b7
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-17 12:27:24 +0100

    ex11

---
+++ Repo09/ex11/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+function functionElse(num){
+    if(num <= 5){
+        var result = "5 or smaller";
+    } else {
+        var result = "Bigger than 5";
+    }
+    return result;
+}
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+// Only change code above this line
+module.exports = functionElse;
