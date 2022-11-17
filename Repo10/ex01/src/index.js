240ba97fff39142972d77c2ad18cf6b78cb2a723
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-17 14:51:25 +0100

    ex01

---
+++ Repo10/ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Dog",
+    name: "Rex",
+    legs: 4,
+    friends: ["Silvester", "Garfield"]
+};
+function myFunction(myObj){
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
