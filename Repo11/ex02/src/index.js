14d078443391a35e4a23f951ea6c2b5f5c609f1c
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-21 12:44:43 +0100

    ex02

--- Repo11/ex02/src/index.js
+++ Repo11/ex02/src/index.js
@@ -1,8 +1,16 @@
 // Only change code below this code
 var myStr = "Paragon is the best!";
 function useMethods(str) {
-  var myReverse = str.split("").reverse().join('');
-  var countCharacters = str.split(" ").map(x => x.length).reverse();
+  var myReverse
+    = str
+      .split("")
+      .reverse()
+      .join('');
+  var countCharacters
+    = str
+      .split(" ")
+      .map(x => x.length)
+      .reverse();
   return { myReverse, countCharacters };
 }
 console.log(useMethods(myStr));
