19c7458f11d0f29612dc92c0e395197977710647
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-22 21:14:38 +0100

    ex02

--- Repo12/ex02/src/index.js
+++ Repo12/ex02/src/index.js
@@ -1,4 +1,4 @@
-// Only chance code below this line
+// Only change code below this line
 function myCounter(n) {
     if(n < 1){
         return [];
@@ -8,7 +8,7 @@ function myCounter(n) {
         return countArray;
     }
 }
-// Only chance code above this line
+// Only change code above this line
 console.log(myCounter(-1));
 console.log(myCounter(10));
 console.log(myCounter(5));
