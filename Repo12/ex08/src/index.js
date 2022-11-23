fb3c65af0fac0a729e69addb2fcf48078651e5da
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-23 01:12:40 +0100

    ex08

---
+++ Repo12/ex08/src/index.js
@@ -0,0 +1,44 @@
+var contacts = [
+    {
+        "firstName": "John",
+        "lastName": "Doe",
+        "number": "12365495208",
+        "likes": ["C", "C++", "C#"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Doe",
+        "number": "4568295761",
+        "likes": ["Python", "Swift", "R"] 
+    },
+    {
+        "firstName": "Richard",
+        "lastName": "Roe",
+        "number": "6938257149",
+        "likes": ["React", "Angular", "Vue"]
+    },
+    {
+        "firstName": "Jane",
+        "lastName": "Roe",
+        "number": "unknown",
+        "likes": ["JavaScript", "Node", "HTML & CSS"]
+    },
+
+];
+function lookUpProfile(name, prop) {
+// Only change code below this line
+    for(var i = 0; i < contacts.length; i++) {
+        if(contacts[i].firstName == name) {
+            if(contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            } else { return "No such property"; }
+        } else //{return "No such contact";}
+    }    
+// Only change code above this line
+}
+console.log(lookUpProfile("John", "likes"));
+console.log(lookUpProfile("Jane", "lastName"));
+console.log(lookUpProfile("Rob", "number"));
+console.log(lookUpProfile("Rob", "JavaScript"));
+console.log(lookUpProfile("John", "address"));
+module.exports = lookUpProfile;
