a02f5b039f47b84f02e715f198987dac906c38b1
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-30 16:05:05 +0100

    ex05

---
+++ Repo15/ex05/src/index.js
@@ -0,0 +1,8 @@
+// only change the code below this line
+const arena = () => {
+// only change the code above this line
+    "use strict";
+    return new Date();
+};
+console.log(arena());
+module.exports = arena;
