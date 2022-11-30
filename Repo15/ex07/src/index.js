7ae3ae7acabae61f917123fba376d705ec049321
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-30 16:18:13 +0100

    ex07

---
+++ Repo15/ex07/src/index.js
@@ -0,0 +1,6 @@
+// only change the code below this line
+const sum = (num1, num2 = 1) => num1 + num2;
+console.log(sum(4, 6));
+console.log(sum(3));
+// only change the code above this line
+module.exports = sum;
