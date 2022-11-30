c4b99c50330ea1787c7aafe10917fbff1db73b66
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-30 15:58:22 +0100

    ex04

--- Repo15/ex04/src/index.js
+++ Repo15/ex04/src/index.js
@@ -2,13 +2,13 @@
 const milili = [10, 25, 40];
 
 function myArr(mili) {
-    "use strict";
-    milili[0] = 4;
-    milili[1] = 10;
-    milili[2] = 25;
-    return milili;
+  "use strict";
+  milili[0] = 4;
+  milili[1] = 10;
+  milili[2] = 25;
+  return milili;
 }
-  // Using mili = [4, 10, 25] would be invalid
+// Using mili = [4, 10, 25] would be invalid
 console.log(myArr(milili));
 // only change the code above this line
 module.exports = myArr;
