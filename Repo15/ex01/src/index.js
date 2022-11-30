c212ed77e2067cf8a4894943e0eebe9b014ac796
Gordan Kranjcic <gordankranjcic@gmail.com>
2022-11-30 10:42:06 +0100

    ex01

---
+++ Repo15/ex01/src/index.js
@@ -0,0 +1,14 @@
+// only change the code below this line
+let myName;
+let message;
+// only change the code above this line
+
+myName = "Padawans";
+function myMsg(name){
+    "use strict";
+    message = "Hello, " + name + "!";
+
+    return message;
+}
+console.log(myMsg(myName));
+module.exports = myMsg;
